4c717d1d6501e163a340ffe3fa107a7ecb27f38f
dsmv04 <dalton.munoz.v@hotmail.com>
2023-10-11 19:42:12 -0300

    Asignacion 4 dia 2 semana 6

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[]
+            {
+                23, 34, 12, 51, 1, 2, -1, -244, 1000
+            };
+            // Example of calling Basic13 Methods
+            int[] odds = Excercises.OddArray();
+            object[] objArr = Excercises.NumToString(testArray);
+
+
+        }
+    }
+}
